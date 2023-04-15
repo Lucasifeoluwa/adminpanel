ee770c201aca97ee1670bd1aa8bfadce0073c9e0
lucasifeoluwa <lucasifeoluwa@yahoo.com>
2023-04-15 10:46:15 +0100

    Widget

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,53 @@
+import {
+  ChatBubbleOutline,
+  DarkModeOutlined,
+  FullscreenExitOutlined,
+  LanguageOutlined,
+  ListOutlined,
+  NotificationsNoneOutlined,
+  SearchOutlined,
+} from "@mui/icons-material";
 import "./navbar.scss";
 
+import img from "../../images/gojo.jpg";
+
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchOutlined />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlined className="icon" />
+            English
+          </div>
+          <div className="item">
+            <DarkModeOutlined className="icon" />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlined className="icon" />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlined className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutline className="icon" />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListOutlined className="icon" />
+          </div>
+          <div className="item">
+            <img src={img} alt="" className="avatar" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,18 @@
 import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonIcon from "@mui/icons-material/Person";
+import ProductionQuantityLimitsIcon from "@mui/icons-material/ProductionQuantityLimits";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import {
+  AccountCircle,
+  Assessment,
+  Fitbit,
+  LocalShipping,
+  Logout,
+  MonitorHeart,
+  Notifications,
+  Settings,
+} from "@mui/icons-material";
 
 const Sidebar = () => {
   return (
@@ -9,21 +23,65 @@ const Sidebar = () => {
       <hr />
       <div className="center">
         <ul>
+          <p className="title">MAIN</p>
           <li>
+            <DashboardIcon className="icon" />
             <span>Dashboard</span>
           </li>
+          <p className="title">LISTS</p>
           <li>
-            <span>Dashboard</span>
+            <PersonIcon className="icon" />
+            <span>Users</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <ProductionQuantityLimitsIcon className="icon" />
+            <span>Products</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <CreditCardIcon className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShipping className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+            <Assessment className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <Notifications className="icon" />
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <MonitorHeart className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <Fitbit className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <Settings className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountCircle className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <Logout className="icon" />
+            <span>Logout</span>
           </li>
         </ul>
       </div>
-      <div className="bottom">color options</div>
+      <div className="bottom">
+        <div className="colorOptions"></div>
+        <div className="colorOptions"></div>
+      </div>
     </div>
   );
 };
